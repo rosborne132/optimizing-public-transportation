1bd9b17948658425a307d9b407ee72a5535658b9
ozborne132@gmail.com <ozborne132@gmail.com>
2024-08-19 19:50:05 -0700

    finish producers

--- producers/connector.py
+++ producers/connector.py
@@ -20,54 +20,45 @@ def configure_connector():
         logging.debug("connector already created skipping recreation")
         return
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-        return
-
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
     logger.info("connector code not completed skipping connector creation")
-    #resp = requests.post(
-    #    KAFKA_CONNECT_URL,
-    #    headers={"Content-Type": "application/json"},
-    #    data=json.dumps({
-    #        "name": CONNECTOR_NAME,
-    #        "config": {
-    #            "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-    #            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "key.converter.schemas.enable": "false",
-    #            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "value.converter.schemas.enable": "false",
-    #            "batch.max.rows": "500",
-    #            # TODO
-    #            "connection.url": "",
-    #            # TODO
-    #            "connection.user": "",
-    #            # TODO
-    #            "connection.password": "",
-    #            # TODO
-    #            "table.whitelist": "",
-    #            # TODO
-    #            "mode": "",
-    #            # TODO
-    #            "incrementing.column.name": "",
-    #            # TODO
-    #            "topic.prefix": "",
-    #            # TODO
-    #            "poll.interval.ms": "",
-    #        }
-    #    }),
-    #)
 
-    ## Ensure a healthy response was given
-    #resp.raise_for_status()
-    #logging.debug("connector created successfully")
+    # Send a POST request to the Kafka Connect URL to create a new connector.
+    resp = requests.post(
+        KAFKA_CONNECT_URL,
+        headers={
+            "Content-Type": "application/json"
+        },
+        data=json.dumps({
+            "name": CONNECTOR_NAME,
+            "config": {
+                "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+                "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "key.converter.schemas.enable": "false",
+                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                "value.converter.schemas.enable": "false",
+                "batch.max.rows": "500",
+                "connection.url": "jdbc:postgresql://localhost:5432/cta",
+                "connection.user": "cta_admin",
+                "connection.password": "chicago",
+                "table.whitelist": "stations",
+                "mode": "incrementing",
+                "incrementing.column.name": "stop_id",
+                "topic.prefix": "postgre_connect_",
+                "poll.interval.ms": "10000",
+            }
+        }),
+    )
+
+    try:
+        resp.raise_for_status()
+        logging.debug("connector created successfully")
+    except:
+        logging.debug("connector creation failed")
+        logger.error(
+            "Failed to create connector: status code %s, message: %s",
+            resp.status_code,
+            resp.content,
+        )
 
 
 if __name__ == "__main__":
--- producers/models/weather.py
+++ producers/models/weather.py
@@ -30,16 +30,16 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # Construct the topic name dynamically based on the month.
+        topic_name = f"weather.{month}"
+
+        # Call the Producer's constructor, passing in the topic name, key schema, and value schema.
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            topic_name,
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=1,  # Number of partitions for the Kafka topic. Adjust for scalability and performance.
+            num_replicas=1  # Number of replicas for fault tolerance. Increase to ensure data availability.
         )
 
         self.status = Weather.status.sunny
@@ -53,9 +53,6 @@ class Weather(Producer):
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
                 Weather.key_schema = json.load(f)
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
@@ -73,37 +70,36 @@ class Weather(Producer):
     def run(self, month):
         self._set_weather(month)
 
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        resp = requests.post(
+           f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+           headers={"Content-Type": "application/vnd.kafka.json.v2+json"},
+           data=json.dumps(
+               {
+                    "key_schema": json.dumps(Weather.key_schema),
+                    "value_schema": json.dumps(Weather.value_schema),
+                    records: [
+                        {
+                            "key": {
+                                "timestamp": self.time_millis()
+                            },
+                            "value": {
+                                "temperature": self.temp,
+                                "status": self.status.name,
+                            }
+                        }
+                    ]
+               }
+           )
+        )
+
+        try:
+            resp.raise_for_status()
+        except:
+            logger.warning(
+                "Failed to send data to kafka, temp: %s, status: %s",
+                self.temp,
+                self.status.name
+            )
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
